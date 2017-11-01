b47c8def193b87a9e300428d571307e238928c4d
erikwilson <erik.e.wilson@gmail.com>
2017-11-01 16:17:58 -0700

    v1.7.1 put a function on it

--- command.js
+++ command.js
@@ -50,7 +50,7 @@ class MeshbluConnectorInstallerWindowsMSICommand {
     })
   }
 
-  async run() {
+  async function run() {
     const { connectorPath, certPassword, destinationPath, userInstall } = this.octoDash.parseOptions()
     const spinner = ora("Building package").start()
     const installer = new MeshbluConnectorInstaller({
